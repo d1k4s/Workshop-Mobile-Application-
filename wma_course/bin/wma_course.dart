c06e88937f9681d1589570e93dc2c80bdf945138
d1k4s <dikaest09@gmail.com>
2022-09-19 08:15:56 +0700

    first commit

---
+++ wma_course/bin/wma_course.dart
@@ -0,0 +1,5 @@
+import 'package:wma_course/wma_course.dart' as wma_course;
+
+void main(List<String> arguments) {
+  print('Hello world: ${wma_course.calculate()}!');
+}
---
+++ wma_course/test/wma_course_test.dart
@@ -0,0 +1,8 @@
+import 'package:wma_course/wma_course.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
